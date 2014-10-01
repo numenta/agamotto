3714eb84ae813ae95733fb9f62e218b9592f2057
Joe Block <jpb@numenta.com>
2014-09-30 23:32:25 -0700

    Fix license and add test for CVE-2014-6278

--- examples/test_shellshock.py
+++ examples/test_shellshock.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # ----------------------------------------------------------------------
-# Copyright (C) 2014 Numenta Inc. All rights reserved.
+# Copyright (C) 2014 Numenta
 #
-# The information and source code contained herein is the
-# exclusive property of Numenta Inc.  No part of this software
-# may be used, reproduced, stored or distributed in any form,
-# without explicit written authorization from Numenta Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # ----------------------------------------------------------------------
 """
-Test shellshock and the other bash bugs
+Test shellshock and other related bash bugs
 """
 import agamotto
 import unittest2 as unittest
@@ -19,48 +26,69 @@ class TestForBashSecurityBugs(unittest.TestCase):
   def testForBashCVE_2014_6271(self):
     """Is bash immune to CVE-2014-6271?
     """
-    self.assertFalse(agamotto.process.stdoutContains("(env x='() { :;}; echo vulnerable'  bash -c \"echo this is a test\") 2>&1",
-                                                     'vulnerable'),
+    test6271 = "(env x='() { :;}; echo vulnerable' " + \
+      " bash -c \"echo this is a test\") 2>&1"
+    self.assertFalse(agamotto.process.stdoutContains(test6271, 'vulnerable'),
                      'Bash is vulnerable to CVE-2014-6271')
-
-
-  def testForBashCVE_2014_7169(self):
-    """Is bash immune to CVE-2014-7169?
-    """
-    self.assertFalse(agamotto.process.stdoutContains("env X='() { (a)=>\' bash -c \"echo echo vuln\"; [[ \"$(cat echo)\" == \"vuln\" ]] && echo \"still vulnerable :(\" 2>&1",
-                                                     'still vulnerable'),
-                     'Bash is vulnerable to CVE-2014-7169')
 
 
   def testForBashCVE_2014_6277(self):
     """Is bash immune to CVE-2014-6277?
     """
-    self.assertFalse(agamotto.process.stdoutContains("foo='() { echo still vulnerable; }' bash -c foo 2>&1",
+    test6277 = "foo='() { echo still vulnerable; }' bash -c foo 2>&1"
+    self.assertFalse(agamotto.process.stdoutContains(test6277,
+                                                     'still vulnerable'),
+                     'Bash is vulnerable to CVE-2014-6277')
+
+
+  def testForBashCVE_2014_6278(self):
+    """Is bash immune to CVE-2014-6278?
+    """
+    test6278 = "shellshocker='() { echo You are vulnerable; }' " + \
+      "bash -c shellshocker"
+    self.assertFalse(agamotto.process.stdoutContains(test6278, 'vulnerable'),
+                     'Bash is vulnerable to CVE-2014-6278')
+
+
+  def testForBashCVE_2014_7169(self):
+    """Is bash immune to CVE-2014-7169?
+    """
+    testFor7169 = "env X='() { (a)=>\' bash -c \"echo echo vuln\";" + \
+      " [[ \"$(cat echo)\" == \"vuln\" ]] && " + \
+      "echo \"still vulnerable :(\" 2>&1"
+    self.assertFalse(agamotto.process.stdoutContains(testFor7169,
                                                      'still vulnerable'),
                      'Bash is vulnerable to CVE-2014-7169')
 
 
   def testForBashCVE_2014_7186_a(self):
     """Is bash immune to CVE-2014-7186 using test from shellshocker.net?"""
-    self.assertFalse(agamotto.process.stdoutContains("bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' || echo 'CVE-2014-7186 vulnerable, redir_stack'",
-                                                     'CVE-2014-7186 vulnerable, redir_stack'),
-                     'Vulnerable to CVE-2014-7186')
+    test7186shellshocker = "bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF " + \
+      "<<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' ||" + \
+      " echo 'CVE-2014-7186 vulnerable, redir_stack'"
+    self.assertFalse(agamotto.process.stdoutContains(test7186shellshocker,
+                                                     'vulnerable'),
+                     'Vulnerable to CVE-2014-7186, redir_stack')
 
 
   def testForBashCVE_2014_7186_b(self):
     """Is bash immune to CVE-2014-7186 using eblake@redhat.com's test?"""
     # Try Eric Blake's test too
-    self.assertTrue(agamotto.process.stdoutContains('bash -c "export f=1 g=\'() {\'; f() { echo 2;}; export -f f; bash -c \'echo \$f \$g; f; env | grep ^f=\'"',
-                                                    "f=1"),
-                    "Fails Eric Blake's 7186 test")
+    blake7186Test = 'bash -c "export f=1 g=\'() {\'; f() { echo 2;}; ' + \
+      'export -f f; bash -c \'echo \$f \$g; f; env | grep ^f=\'"'
+    self.assertTrue(agamotto.process.stdoutContains(blake7186Test, "f=1"),
+                    "Fails Eric Blake's CVE-2014-7186 test")
 
 
   def testForBashCVE_2014_7187(self):
     """Is bash immune to CVE-2014-7187 using test from shellshocker.net?"""
-    self.assertFalse(agamotto.process.stdoutContains('(for x in {1..200} ; do echo "for x$x in ; do :"; done; for x in {1..200} ; do echo done ; done) | bash || echo "CVE-2014-7187 vulnerable, word_lineno"',
-                                                     'CVE-2014-7187 vulnerable, word_lineno'),
+    test7187 = '(for x in {1..200} ; do echo "for x$x in ; do :"; done; ' + \
+      'for x in {1..200} ; do echo done ; done) | bash || ' + \
+      'echo "CVE-2014-7187 vulnerable, word_lineno"'
+    self.assertFalse(agamotto.process.stdoutContains(test7187, 'vulnerable'),
                      'CVE-2014-7187 vulnerable, word_lineno')
 
 
 if __name__ == '__main__':
   unittest.main()
+
